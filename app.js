795614c2d68ecd613b670ce3c5e6ac0fe025b3c0
ahmedabukar01 <ahmedlearn01@gmail.com>
2021-01-12 09:12:50 +0300

    ok

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-const liveButton = document.getElementsByClassName('.live-button');
+const liveButton = document.querySelector('.live-button');
+const output = document.querySelector('.output');
+const input = document.querySelector('form input');
 
-liveButton.addEventListiner('onkeyup', liveTyping)
+liveButton.addEventListener('click', liveOutput);
+input.addEventListener('keyup', e=>{
+    let text = input.value;
+    liveTyping(text);
+});
+
+function liveOutput(){
+    output.textContent ="here you see your typing";
+    output.style.display="block";
+    input.style.display="inline";
+}
+// live typing
+function liveTyping(text){
+    output.textContent = text;
+}
